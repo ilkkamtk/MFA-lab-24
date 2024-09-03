f252740888265dfbdae8840c6a35128dccc08b16
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2024-09-03 11:13:33 +0300

    update

--- src/api/controllers/twoFAController.ts
+++ src/api/controllers/twoFAController.ts
@@ -1,7 +1,11 @@
 import {NextFunction, Request, Response} from 'express';
 import CustomError from '../../classes/CustomError';
 import {User} from '@sharedTypes/DBTypes';
+import {UserResponse} from '@sharedTypes/MessageTypes';
 import fetchData from '../../utils/fetchData';
+import OTPAuth from 'otpauth';
+import twoFAModel from '../models/twoFAModel';
+import QRCode from 'qrcode';
 
 // TODO: Define setupTwoFA function
 const setupTwoFA = async (
@@ -18,18 +22,39 @@ const setupTwoFA = async (
       },
       body: JSON.stringify(req.body),
     };
-    const userResponse = await fetchData(
+    const userResponse = await fetchData<UserResponse>(
       process.env.AUTH_URL + '/api/v1/users',
       options,
     );
 
-    console.log('userResponse', userResponse);
+    // console.log('userResponse', userResponse);
 
-    res.json({qrCodeUrl: 'moro'});
-    // TODO: Generate a new 2FA secret
-    // TODO: Create the TOTP instance
-    // TODO: Store or update the 2FA data in the database
-    // TODO: Generate a QR code and send it in the response
+    // Generate a new 2FA secret
+    const secret = new OTPAuth.Secret();
+
+    // Create the TOTP instance
+    const totp = new OTPAuth.TOTP({
+      issuer: 'ElukkaAPI',
+      label: userResponse.user.email,
+      algorithm: 'SHA1',
+      digits: 6,
+      period: 30,
+      secret: secret,
+    });
+
+    // Store or update the 2FA data in the database
+    await twoFAModel.create({
+      email: userResponse.user.email,
+      userId: userResponse.user.user_id,
+      twoFactorEnabled: true,
+      twoFactorSecret: secret,
+    });
+
+    // Generate a QR code and send it in the response
+    console.log('totp', totp.toString());
+    const imageUrl = await QRCode.toDataURL(totp.toString());
+
+    res.json({qrCodeUrl: imageUrl});
   } catch (error) {
     next(new CustomError((error as Error).message, 500));
   }
